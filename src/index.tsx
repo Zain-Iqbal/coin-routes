3642215eaf00930b045e5bdaea16dd6042852593
zainiqbal <shazeel@tncitgroup.com>
2024-06-12 15:59:02 +0400

    routes changing

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import {Provider} from 'react-redux'
-import {
-    createHashRouter,
-    RouterProvider
-} from 'react-router-dom';
 
 import App from './App';
 import ThemeProvider from "./theme/theme-provider";
@@ -17,19 +13,12 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-const router = createHashRouter([
-    {
-        path: "/*",
-        element: <App />,
-    }
-]);
-
 root.render(
         <ThemeProvider>
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
-                    <RouterProvider router={router} />
+                        <App />
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </ThemeProvider>
 );
